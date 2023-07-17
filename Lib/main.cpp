441203f3dc45eb03d144212be29f58600b8f2747
ChloeVZ <58456478+ChloeZamorano@users.noreply.github.com>
2023-07-16 19:27:27 -0700

    It doesn't work but I wanna commit something

--- Lib/main.cpp
+++ Lib/main.cpp
@@ -1,28 +1,50 @@
 #include "pch.hpp"
 
-#include "HE/HE2/Reflection.hpp"
+#include "HE/Bina/Bina.hpp"
+#include "Devil/Log.hpp"
 
 using namespace fln;
+using namespace dvl;
 
 i32 main()
 {
-	auto file = he2::Reflection::Load("./player_common.rfl").Expect();
+	//auto file = he::BinaDescriptor::Load("./TestFiles/HE2/RFL/player_common.rfl");
 
-	std::vector<cstr> str = file.GetStrings();
-	std::vector<u8*> ptr = file.GetPointers();
+	u8 sn = 69;
+	u8 array[7]
+	{
+		'A',
+		58,
+		'P',
+		'P',
+		255,
+		0,
+		'E',
+	};
+	u32 inty = 420;
+	std::vector<std::pair<u8*, u64>> var
+	{
+		{ (u8*)&sn, sizeof(sn) },
+		{ (u8*)&array, sizeof(array) },
+		{ (u8*)&inty, sizeof(inty) },
+	};
+	std::vector<cstr> str
+	{
+		"Hi hello there :3",
+		"Cutie Clyde",
+		"God he's hot",
+	};
+	cstr secret = "This is totally not a super secret message :0";
 
-	std::cout <<
-		"player_common.rfl\n" <<
-		"	Strings:\n";
-	for (u32 i = 0; i < str.size(); ++i)
-		std::cout << "		" << str[i] << "\n";
+	auto bina = he::BinaDescriptor::Build(
+		var,
+		str,
+		(u8*)secret, strlen(secret));
 
-	std::cout <<
-		"	Bytes:\n" << std::hex << std::uppercase;
-	for (u32 i = 0; i < ptr.size(); ++i)
-		std::cout << "		" << std::setfill('0') << std::setw(2) << (u32)*ptr[i] << "\n";
-
-	std::cout << std::dec << std::nouppercase << "EOF";
+	//FILE* file = fopen("./nstdout.bin", "wb");
+	//if(!file) return 69;
+	//fwrite(file.m_Header, 1, file.m_FileSize, file);
+	//fclose(file);
 
 	return 0;
 }
